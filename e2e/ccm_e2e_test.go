04d5c3b1795cd65b30d08763b755aa42ecae4af0
vochidat0 <74147122+chpiano2000@users.noreply.github.com>
2024-03-18 16:43:58 +0700

    test nip.io
    * feat: add double quotes
    
    * feat: nip.io ingress test suit
    
    * feat: remove ensureLB for delete ingress
    
    * hotfix: change double quotes to single quotes

--- e2e/ccm_e2e_test.go
+++ e2e/ccm_e2e_test.go
@@ -4,12 +4,10 @@ import (
 	"e2e_test/test/framework"
 	"fmt"
 
-	"github.com/bizflycloud/gobizfly"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
 	core "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
@@ -58,31 +56,13 @@ var _ = Describe("CCM E2E Tests", func() {
 		Eventually(watcher.ResultChan()).Should(Receive(EnsuredService(isDeleted)))
 	}
 
-	createPodWithLabel := func(pods []string, ports []core.ContainerPort, image string, labels map[string]string, selectNode bool) {
-		for i, pod := range pods {
-			p := f.LoadBalancer.GetPodObject(pod, image, ports, labels)
-			if selectNode {
-				p = f.LoadBalancer.SetNodeSelector(p, workers[i])
-			}
-			Expect(f.LoadBalancer.CreatePod(p)).ToNot(BeNil())
-			Eventually(f.LoadBalancer.GetPod).WithArguments(p.ObjectMeta.Name, f.LoadBalancer.Namespace()).Should(HaveField("Status.Phase", Equal(core.PodRunning)))
-		}
+	deployIngressController := func(namespace string) {
+		Expect(f.LoadBalancer.DeployIngressController()).NotTo(HaveOccurred())
+		ensureServiceLoadBalancer(namespace, false)
 	}
 
-	deletePods := func(pods []string) {
-		for _, pod := range pods {
-			Expect(f.LoadBalancer.DeletePod(pod)).NotTo(HaveOccurred())
-		}
-	}
-
-	deleteService := func() {
-		Expect(f.LoadBalancer.DeleteService()).NotTo(HaveOccurred())
-	}
-
-	createServiceWithAnnotations := func(labels, annotations map[string]string, ports []core.ServicePort, isSessionAffinityClientIP bool, isDelete bool) {
-		Expect(f.LoadBalancer.CreateService(labels, annotations, ports, isSessionAffinityClientIP)).NotTo(HaveOccurred())
-		Eventually(f.LoadBalancer.GetServiceEndpoints).Should(Not(BeEmpty()))
-		ensureServiceLoadBalancer("", isDelete)
+	destroyIngressController := func() {
+		Expect(f.LoadBalancer.UninstallIngressController()).NotTo(HaveOccurred())
 	}
 
 	Describe("Test", func() {
@@ -91,89 +71,29 @@ var _ = Describe("CCM E2E Tests", func() {
 				err := root.Recycle()
 				Expect(err).NotTo(HaveOccurred())
 			})
-			Context("Load Balancer target node label", func() {
-				var (
-					pods        []string
-					labels      map[string]string
-					annotations = map[string]string{}
-				)
+			Context("Load Balancer with ingress hostname", func() {
+				namespace := "ingress-nginx"
 
 				BeforeEach(func() {
-					pods = []string{"test-pod-1", "test-pod-2"}
-					ports := []core.ContainerPort{
-						{
-							Name:          "http-1",
-							ContainerPort: 8080,
-						},
-					}
-					servicePorts := []core.ServicePort{
-						{
-							Name:       "http-1",
-							Port:       80,
-							TargetPort: intstr.FromInt(80),
-							Protocol:   "TCP",
-						},
-						{
-							Name:       "https-1",
-							Port:       443,
-							TargetPort: intstr.FromInt(80),
-							Protocol:   "TCP",
-						},
-					}
-					labels = map[string]string{
-						"app": "test-loadbalancer",
-					}
-					annotations[bizflyNodeLabel] = "test-ccm=node01"
-
-					By("Creating Pods")
-					createPodWithLabel(pods, ports, framework.TestServerImage, labels, true)
-
-					By("Creating Service")
-					createServiceWithAnnotations(labels, annotations, servicePorts, false, false)
+					namespace = "ingress-nginx"
+					By("Deploying Nginx ingress controller")
+					deployIngressController(namespace)
 				})
 
 				AfterEach(func() {
-					By("Deleting the Pods")
-					deletePods(pods)
-
-					By("Deleting the Service")
-					deleteService()
+					By("Destroying Nginx ingress controller")
+					destroyIngressController()
 				})
 
-				It("Should have internal network type", func() {
-					var lb *gobizfly.LoadBalancer
-					var lbId string
-					var members int
-					var listeners []*gobizfly.Listener
-					var pools []*gobizfly.Pool
+				It("Should have external IP as a hostname", func() {
+					var hostname []string
+					By("Getting hostname")
 					Eventually(func() error {
-						lb, err = f.GetLB(ctx, clusterName, framework.TestServerResourceName)
-						lbId = lb.ID
+						hostname, err = f.LoadBalancer.GetLoadBalancerHostName(namespace, "ingress-nginx-controller")
 						return err
 					}).Should(BeNil())
-					Eventually(func() error {
-						listeners, err = f.GetListners(ctx, lbId)
-						fmt.Println("Listeners %i", len(listeners))
-						return err
-					}).Should(BeNil())
-					Eventually(func() error {
-						pools, err = f.GetPools(ctx, lbId)
-						fmt.Println("Pools %i", len(pools))
-						return err
-					}).Should(BeNil())
-					Eventually(func() error {
-						members, err = f.CountMembersByPools(ctx, pools)
-						fmt.Println("Members %i", members)
-						return err
-					}).Should(BeNil())
-					Eventually(lbId).ShouldNot(Equal(""))
-					By("Checking numbers of Listners")
-					Eventually(len(listeners)).Should(Equal(2))
-					By("Checking numbers of Pools")
-					Eventually(len(pools)).Should(Equal(2))
-					By("Checking numbers of Members")
-					Eventually(members).Should(Equal(2))
-					By("Checking Pool Protocol")
+					By("Checking for .nip.io domain")
+					Eventually(hostname[0]).Should(ContainSubstring(".nip.io"))
 				})
 			})
 		})
